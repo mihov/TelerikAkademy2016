ee68308ab8f6c5d7a95b5d4d9b39510107430127
mihov <tancho.mihov@gmail.com>
2016-03-31 23:18:31 +0300

    First Git

---
+++ CSharp-Part-1/01. Introduction-to-Programming/09. Print Sequence/PrintSequence.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class PrintSequence
+{
+    static void Main()
+    {
+        bool sign = true;
+        for (int counter = 1; counter < 11; counter++)
+        {
+            if ((counter % 2) == 0)
+            {
+                Console.WriteLine("-" + (counter + 1));
+            }
+            else
+            {
+
+                Console.WriteLine((counter + 1));
+            }
+
+        }
+    }
+}
---
+++ CSharp-Part-1/01. Introduction-to-Programming/15. Age/Age.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Age
+{
+    static void Main()
+    {
+        string yearAsStr, readBDayAsStr = Console.ReadLine();
+
+        yearAsStr = readBDayAsStr.Substring(readBDayAsStr.Length - 4);
+
+        int birthYear = int.Parse(yearAsStr);
+        int ageNow, ageAfter10Y;
+
+        ageNow = DateTime.Now.Year - birthYear;
+        ageAfter10Y = ageNow + 10;
+
+        Console.WriteLine(ageNow);
+        Console.WriteLine(ageAfter10Y);
+    }
+}
