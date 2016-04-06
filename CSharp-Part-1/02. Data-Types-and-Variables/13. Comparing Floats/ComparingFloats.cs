3b37bf3572f864630dfe59346a83ae3c04b115a5
mihov <tancho.mihov@gmail.com>
2016-04-06 23:05:01 +0300

    All done 100/100

---
+++ CSharp-Part-1/02. Data-Types-and-Variables/13. Comparing Floats/ComparingFloats.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class ComparingFloats
+{
+    static void Main()
+    {
+        double eps = 0.000001;
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+
+        Console.WriteLine((Math.Abs(a - b) < eps).ToString().ToLower());
+    }
+}
