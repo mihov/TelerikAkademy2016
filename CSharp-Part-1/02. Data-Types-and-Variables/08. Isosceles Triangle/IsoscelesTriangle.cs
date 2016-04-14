55240adaf0643fdf40ba0be65a08b0bd0d6723f7
mihov <tancho.mihov@gmail.com>
2016-04-15 01:16:15 +0300

    More old things to commit

---
+++ CSharp-Part-1/02. Data-Types-and-Variables/08. Isosceles Triangle/IsoscelesTriangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Isosceles_Triangle
+{
+    class IsoscelesTriangle
+    {
+        static void Main()
+        {
+            Console.WriteLine(string.);
+        }
+    }
+}
