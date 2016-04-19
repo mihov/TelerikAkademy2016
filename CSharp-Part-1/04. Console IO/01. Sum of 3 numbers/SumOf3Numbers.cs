a4567ba26635f942d7f4c9c0b4eaf0d43bc945e3
mihov <tancho.mihov@gmail.com>
2016-04-19 17:02:34 +0300

    add 1 & 2

---
+++ CSharp-Part-1/04. Console IO/01. Sum of 3 numbers/SumOf3Numbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+class SumOf3Numbers
+{
+    static void Main()
+    {
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+        float a = float.Parse(Console.ReadLine());
+        float b = float.Parse(Console.ReadLine());
+        float c = float.Parse(Console.ReadLine());
+
+        Console.WriteLine(a + b + c);
+    }
+}
