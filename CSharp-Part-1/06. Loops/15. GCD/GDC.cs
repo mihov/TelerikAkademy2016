105a0efe25854570c2b5b94769aef39281ed761f
mihov <tancho.mihov@gmail.com>
2016-04-26 23:45:51 +0300

    All done

---
+++ CSharp-Part-1/06. Loops/15. GCD/GDC.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class GDC
+{
+    static void Main()
+    {
+        {
+            var input = Console.ReadLine().Split(' ');
+            int a = Math.Abs(int.Parse(input[0]));
+            int b = Math.Abs(int.Parse(input[1])); ;
+
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+
+            if (a == 0)
+                Console.WriteLine(b);
+            else
+                Console.WriteLine(a);
+        }
+    }
+}
