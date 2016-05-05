0c120b0a9703c84dfa92b15a11124909e44d7747
Tancho Mihov <tanchomihov@gmail.com>
2016-05-05 12:56:00 +0300

    4 done

--- CSharp-Part-2/01. Arrays/04. Maximal sequence/MaximalSequence.cs
+++ CSharp-Part-2/01. Arrays/04. Maximal sequence/MaximalSequence.cs
@@ -8,5 +8,35 @@ class MaximalSequence
 {
     static void Main()
     {
+        int n = int.Parse(Console.ReadLine());
+        int[] intArrayA = new int[n];
+
+        int maxSeq = 1, tempMaxSeq = 1;
+ 
+
+        for (int i = 0; i < n; i++)
+        {
+            intArrayA[i] = int.Parse(Console.ReadLine());
+        }
+
+        int currentElement = intArrayA[0];
+
+        for (int i = 1; i < n; i++)
+        {
+            if (intArrayA[i] == intArrayA[i-1])
+            {
+                tempMaxSeq++;
+            }
+            else
+            {
+                if (tempMaxSeq > maxSeq)
+                {
+                    maxSeq = tempMaxSeq;
+                }
+            }
+        }
+
+        Console.WriteLine(maxSeq);
+
     }
 }
