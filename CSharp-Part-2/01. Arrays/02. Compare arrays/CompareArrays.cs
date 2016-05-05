1e0dc89d4b16ed5a74a648e08f8a02e19ef25faa
Tancho Mihov <tanchomihov@gmail.com>
2016-05-05 12:45:52 +0300

    2&3 done

---
+++ CSharp-Part-2/01. Arrays/02. Compare arrays/CompareArrays.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class CompareArrays
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] intArrayA = new int[n], intArrayB = new int[n];
+        bool isEqual = true;
+
+        for (int i = 0; i < n; i++)
+        {
+            intArrayA[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            intArrayB[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (intArrayA[i] != intArrayB[i])
+            {
+                isEqual = false;
+            }
+        }
+
+        Console.WriteLine((isEqual) ? "Equal" : "Not equal");
+    }
+}
---
+++ CSharp-Part-2/01. Arrays/03. Compare char arrays/CompareCharArrays.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class CompareCharArrays
+{
+    static void Main()
+    {
+
+        string intArrayA = Console.ReadLine();
+        string intArrayB = Console.ReadLine();
+
+        char theSign = '=';
+
+        int minLength = Math.Min(intArrayA.Length, intArrayB.Length);
+
+        if (intArrayA.Length == intArrayB.Length)
+        {
+            for (int i = 0; i < minLength; i++)
+            {
+                if (intArrayA[i] < intArrayB[i])
+                {
+                    theSign = '<';
+                    break;
+                }
+
+                if (intArrayA[i] > intArrayB[i])
+                {
+                    theSign = '>';
+                    break;
+                }
+            }
+        }
+        else
+        {
+
+            theSign = (intArrayA.Length > intArrayB.Length) ? '>' : '<';
+        }
+
+        Console.WriteLine(theSign);
+    }
+}
