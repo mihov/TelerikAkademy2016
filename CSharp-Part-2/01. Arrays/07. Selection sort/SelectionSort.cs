9904040c1acf86317b31a092f4d8837df812f356
mihov <tancho.mihov@gmail.com>
2016-05-05 15:58:18 +0300

    7done

---
+++ CSharp-Part-2/01. Arrays/07. Selection sort/SelectionSort.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class SelectionSort
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] intArrayA = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            intArrayA[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (int j = 0; j < n - 1; j++)
+        {
+            int iMin = j;
+
+            for (int i = j + 1; i < n; i++)
+            {
+                if (intArrayA[i] < intArrayA[iMin])
+                {
+                    iMin = i;
+                }
+            }
+
+            if (iMin != j)
+            {
+                Swap(intArrayA, j, iMin);
+            }
+        }
+
+        Console.WriteLine(String.Join("\n", intArrayA));
+    }
+
+    static int Swap(int[] theArray, int a, int b)
+    {
+        int temp = theArray[a];
+        theArray[a] = theArray[b];
+        theArray[b] = temp;
+
+        return 0;
+    }
+}
