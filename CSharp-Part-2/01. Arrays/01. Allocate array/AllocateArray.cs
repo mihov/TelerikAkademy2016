a9930fae8d3ad3ff2e6eb5c8dcda7f2081f7cd93
Tancho Mihov <tanchomihov@gmail.com>
2016-05-05 11:57:12 +0300

    Task 1 added

---
+++ CSharp-Part-2/01. Arrays/01. Allocate array/AllocateArray.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class AllocateArray
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] intArray = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            intArray[i] = i * 5;
+        }
+
+        Console.WriteLine(String.Join("\n", intArray));
+    }
+}
