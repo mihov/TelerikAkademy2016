a35385253096cc6a9f26b19dc6ca70c9974495a6
mihov <tancho.mihov@gmail.com>
2016-05-08 00:54:03 +0300

    Merge Sort added

--- CSharp-Part-2/01. Arrays/08. Maximal sum/MaximalSum.cs
+++ CSharp-Part-2/01. Arrays/08. Maximal sum/MaximalSum.cs
@@ -7,6 +7,29 @@ class MaximalSum
 {
     static void Main()
     {
+        int n = int.Parse(Console.ReadLine());
+        int[] intArrayA = new int[n];
 
+        int currentSum = 0, maxSum = int.MinValue;
+
+        for (int i = 0; i < n; i++)
+        {
+            intArrayA[i] = int.Parse(Console.ReadLine());
+
+            currentSum += intArrayA[i];
+
+            if (currentSum < intArrayA[i])
+            {
+                currentSum = intArrayA[i];
+            }
+
+
+            if (maxSum < currentSum)
+            {
+                maxSum = currentSum;
+            }
+        }
+
+        Console.WriteLine(maxSum);
     }
 }
---
+++ CSharp-Part-2/01. Arrays/13. Merge sort/MergeSort.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class MergeSort
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] intArrayA = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            intArrayA[i] = int.Parse(Console.ReadLine());
+        }
+
+        MergeAndSort(intArrayA, 0, n-1);
+
+        Console.WriteLine(String.Join("\n", intArrayA));
+    }
+
+    public static void MergeAndSort(int[] input, int left, int right)
+    {
+        if (left < right)
+        {
+            int middle = (left + right) / 2;
+
+            MergeAndSort(input, left, middle);
+            MergeAndSort(input, middle + 1, right);
+
+            //Merge
+            int[] leftArray = new int[middle - left + 1];
+            int[] rightArray = new int[right - middle];
+
+            Array.Copy(input, left, leftArray, 0, middle - left + 1);
+            Array.Copy(input, middle + 1, rightArray, 0, right - middle);
+
+            int i = 0;
+            int j = 0;
+            for (int k = left; k < right + 1; k++)
+            {
+                if (i == leftArray.Length)
+                {
+                    input[k] = rightArray[j];
+                    j++;
+                }
+                else if (j == rightArray.Length)
+                {
+                    input[k] = leftArray[i];
+                    i++;
+                }
+                else if (leftArray[i] <= rightArray[j])
+                {
+                    input[k] = leftArray[i];
+                    i++;
+                }
+                else
+                {
+                    input[k] = rightArray[j];
+                    j++;
+                }
+            }
+        }
+    }
+}
