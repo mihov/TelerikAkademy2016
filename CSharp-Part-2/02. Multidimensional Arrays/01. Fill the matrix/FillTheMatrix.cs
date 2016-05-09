306d94e148a4a0b02f6ac825c90bdb67aaa028ee
mihov <tancho.mihov@gmail.com>
2016-05-10 00:32:55 +0300

    1& done

--- CSharp-Part-2/02. Multidimensional Arrays/01. Fill the matrix/FillTheMatrix.cs
+++ CSharp-Part-2/02. Multidimensional Arrays/01. Fill the matrix/FillTheMatrix.cs
@@ -24,14 +24,35 @@ class FillTheMatrix
                         counter++;
                     }
                 }
-
                 PrintMatrix(theMatrix);
                 break;
+
             case 'b':
+                for (int i = 0; i < dimention; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        for (int j = 0; j < dimention; j++)
+                        {
+                            theMatrix[j, i] = counter;
+                            counter++;
+                        }
+                    }
+                    else {
+                        for (int j = (dimention - 1); j >= 0; j--)
+                        {
+                            theMatrix[j, i] = counter;
+                            counter++;
+                        }
+                    }
+                }
+                PrintMatrix(theMatrix);
                 break;
             case 'c':
+                PrintMatrix(FillC(dimention));
                 break;
             case 'd':
+                PrintMatrix(FillD(dimention));
                 break;
 
             default:
@@ -50,4 +71,133 @@ class FillTheMatrix
 
         }
     }
+
+    static int[,] FillC(int size)
+    {
+        // Calculate elements of the matrix
+        int elements = size * size;
+
+        // Define the matrix
+        int[,] theMatrix = new int[size, size];
+
+        // Set start position
+        int row = size - 1;
+        int col = 0;
+        int counter = 1;
+
+        while (counter <= elements)
+        {
+            // Fill current cell
+            theMatrix[row, col] = counter;
+
+            // Go to next elemet and cell
+            counter++;
+            col++;
+            row++;
+
+            // Diagonal reached 
+            if (row == size && col == size)
+            {
+                // Move to proper cell
+                row = 0;
+                col = 1;
+            }
+
+            // Bottom reached
+            if (row == size)
+            {
+                // Move to proper cell
+                row = size - col - 1;
+                col = 0;
+            }
+
+            // Right end reached
+            if (col == size)
+            {
+                // Move to proper cell
+                col = size - row + 1;
+                row = 0;
+            }
+        }
+        return theMatrix;
+    }
+
+    static int[,] FillD(int size)
+    {
+        int elements = size * size;
+
+        // Define the matrix
+        int[,] theMatrix = new int[size, size];
+
+        // Set start position and direction
+        int row = 0;
+        int col = 0;
+        char direction = 'D';
+        int counter = 1;
+
+        while (counter <= elements)
+        {
+            // Fill current cell
+            theMatrix[row, col] = counter;
+
+            // Go to next element
+            counter++;
+
+            // Go to next proper cell in depend of current direction 
+            switch (direction)
+            {
+                case 'D':
+                    row++;
+                    break;
+                case 'L':
+                    col--;
+                    break;
+                case 'U':
+                    row--;
+                    break;
+                case 'R':
+                    col++;
+                    break;
+                default:
+                    break;
+            }
+
+            // Change the direction if edge of the matrix was reached or the cell is not empty.
+            if ((row < 0) || (row == size) || (col < 0) || (col == size) || theMatrix[row, col] > 0)
+            {
+                switch (direction)
+                {
+                    case 'D':
+                        row--;
+                        col++;
+                        direction = 'R';    // Change the direction to right
+                        break;
+
+                    case 'R':
+                        col--;
+                        row--;
+                        direction = 'U';    // Change the direction to up
+                        break;
+
+                    case 'U':
+                        row++;
+                        col--;
+                        direction = 'L';    // Change the direction to left
+                        break;
+
+                    case 'L':
+                        row++;
+                        col++;
+                        direction = 'D';    // Change the direction to down
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        return theMatrix;
+    }
+
 }
---
+++ CSharp-Part-2/02. Multidimensional Arrays/02. Maximal sum/MaximalSum.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class MaximalSum
+{
+    static void Main()
+    {
+        string[] inputLine = Console.ReadLine().Split(' ');
+
+        int[] matrixDimention = Array.ConvertAll<string, int>(inputLine, int.Parse);
+
+        int row = matrixDimention[0];
+        int col = matrixDimention[1];
+
+        int[,] theMatrix = new int[row, col];
+        int[] tempRow = new int[col];
+
+        int maxSum = int.MinValue, tempMaxSum = 0;
+
+        for (int i = 0; i < row; i++)
+        {
+            inputLine = Console.ReadLine().Split(' ');
+            tempRow = Array.ConvertAll<string, int>(inputLine, int.Parse);
+
+            for (int j = 0; j < col; j++)
+            {
+                theMatrix[i, j] = tempRow[j];
+            }
+        }
+
+        for (int i = 0; i <= row; i++)
+        {
+            for (int j = 0; j <= col; j++)
+            {
+                if (IsInside_3x3(theMatrix, i, j))
+                {
+                    tempMaxSum = CalcMatrix_3x3(theMatrix, i, j);
+                    // Console.WriteLine("[{0},{1}] = {2}", i,j,tempMaxSum);
+                }
+
+
+                if (tempMaxSum > maxSum)
+                {
+                    maxSum = tempMaxSum;
+                }
+
+                tempMaxSum = 0;
+            }
+        }
+
+        Console.WriteLine(maxSum);
+
+        //for (int i = 0; i < row; i++)
+        //{
+        //    for (int j = 0; j < col; j++)
+        //    {
+        //        Console.Write( theMatrix[i, j].ToString().PadLeft(4,' '));
+        //    }
+        //    Console.WriteLine();
+        //}
+
+
+
+    }
+
+    static int CalcMatrix_3x3(int[,] inputMatrix, int coordR, int coorC)
+    {
+        int resultSum = 0;
+
+        for (int i = coordR; i < coordR + 3; i++)
+        {
+            for (int j = coorC; j < coorC + 3; j++)
+            {
+                resultSum += inputMatrix[i, j];
+            }
+        }
+
+        return resultSum;
+    }
+
+    static bool IsInside_3x3(int[,] inputMatrix, int coordR, int coordC)
+    {
+        // bool result = true;
+
+        int rowsMax = inputMatrix.GetLength(0) - 1;
+        int colsMax = inputMatrix.GetLength(1) - 1;
+
+        //if ((coordR + 3 < rowsMax) && (coordC+3 < colsMax))
+        //{
+        //    result = 
+        //}
+
+
+        return (coordR + 2 <= rowsMax) && (coordC + 2 <= colsMax);
+    }
+}
