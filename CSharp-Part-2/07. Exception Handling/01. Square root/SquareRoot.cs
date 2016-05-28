03bd4e07daccc52b5d9ac451fc37be418b9339ef
mihov <tancho.mihov@gmail.com>
2016-05-29 00:06:09 +0300

    Exception HW done

---
+++ CSharp-Part-2/07. Exception Handling/01. Square root/SquareRoot.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class SquareRoot
+{
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("{0:f3}",Sqrt(double.Parse(Console.ReadLine())));
+        }
+        catch(Exception)
+        {
+            Console.WriteLine("Invalid number");
+        }
+        finally
+        {
+            Console.WriteLine("Good bye");
+        }
+
+    }
+
+    static double Sqrt(double value)
+    {
+        if (value < 0)
+            throw new System.ArgumentOutOfRangeException(
+               "Sqrt for negative numbers is undefined!");
+
+        return Math.Sqrt(value);
+    }
+}
---
+++ CSharp-Part-2/07. Exception Handling/02. Enter numbers/EnterNumbers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class EnterNumbers
+{
+    static int start = 1;
+    static int end = 100;
+    const int count = 10;
+
+    static void Main()
+    {
+        List<int> numbers = new List<int>();
+
+        for (int i = start; i <= count; i++)
+        {
+            numbers.Add(int.Parse(Console.ReadLine()));
+        }
+
+        try
+        {
+            if (numbers.Any(x => x < start) || numbers.Any(x => x > end) || !IsIncreasing(numbers))
+            {
+                throw new ArgumentException();
+            }
+
+            Console.WriteLine(start + " < " + string.Join(" < ", numbers) + " < " + end);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Exception");
+        }
+
+    }
+
+    private static bool IsIncreasing(List<int> numbers)
+    {
+        for (int i = 1; i < numbers.Count; i++)
+        {
+            if (numbers[i - 1].CompareTo(numbers[i]) >= 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
