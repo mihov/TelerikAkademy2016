741c64a24134f887f20dd6c1d1042dc39f2dabc5
mihov <tancho.mihov@gmail.com>
2016-06-13 23:37:09 +0300

    LetStart

---
+++ OOP/Defining Classes - Part 2/DefiningClassesPart2/Point3D.cs
@@ -0,0 +1,29 @@
+﻿namespace DefiningClassesPart2
+{
+    using System;
+
+    public struct Point3D
+    {
+        public decimal X { get; set; }
+        public decimal Y { get; set; }
+        public decimal Z { get; set; }
+
+
+
+        public Point3D(decimal xCoor = 0m, decimal yCoor = 0m, decimal zCoor = 0m)
+        {
+            this.X = xCoor;
+            this.Y = yCoor;
+            this.Z = zCoor;
+        }
+
+
+
+        public override string ToString()
+        {
+            return String.Format("[{0}][{1}][{2}]", this.X, this.Y, this.Z);
+        }
+
+
+    }
+}
---
+++ OOP/Defining Classes - Part 2/DefiningClassesPart2/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace DefiningClassesPart2
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class StartUp
+    {
+        static void Main()
+        {
+        }
+    }
+}
