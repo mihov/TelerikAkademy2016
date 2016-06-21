5a579ac653b44636b1ae5057c151e22d55b175f9
Tancho Mihov <tanchomihov@gmail.com>
2016-06-21 13:04:02 +0300

    SomeFixwes

---
+++ OOP/03. Extension-Methods-Delegates-Lambda-LINQ/Problem 03/Student.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_03
+{
+    public class Student
+    {
+        public Student(string firstName, string lastName, int age, string fn, string tel, string email
+            , List<int> marks, int groupNumber)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.FN = fn;
+            this.Tel = tel;
+            this.Email = email;
+            this.Marks = marks;
+            this.GroupNumber = groupNumber;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string FN { get; set; }
+        public string Tel { get; set; }
+        public string Email { get; set; }
+        public List<int> Marks { get; set; }
+        public int GroupNumber { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName}";
+        }
+
+        public static void Print(Student[] studentList)
+        {
+            foreach (var item in studentList)
+            {
+                Console.WriteLine($"{item.FirstName} {item.LastName}");
+            }
+        }
+
+        public static Student[] StudentsInfo()
+        {
+            var result = new Student[4];
+            result[0] = new Student("Stefan", "Ivanov", 17, "123456", "+359 885 236 441", "Stefan.Ivanov@abv.bg", new List<int> { 5, 4, 5, 3, 2 }, 2);
+            result[1] = new Student("Vasil", "Petkov", 18, "123457", "+359 885 444 441", "Vasil.Petkov@mail.bg", new List<int> { 3, 5, 6, 5, 4, 3 }, 3);
+            result[2] = new Student("Kolio", "Georgiev", 23, "123458", "+359 885 123 441", "Kolio.Georgiev@abv.bg", new List<int> { 3, 4, 5, 6, 3, 4 }, 2);
+            result[3] = new Student("Stefan", "Kolev", 27, "1234510", "+359 885 777 444", "Stefan.Kolev@abv.com", new List<int> { 4, 6, 3, 5, 3, 5 }, 1);
+            return result;
+        }
+    }
+}
