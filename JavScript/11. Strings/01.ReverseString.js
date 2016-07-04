d733bf507441538fbf2ed65d0e2857f9943e467f
Tancho Mihov <tanchomihov@gmail.com>
2016-07-04 12:14:35 +0300

    Js HW 1-2

---
+++ JavScript/11. Strings/01.ReverseString.js
@@ -0,0 +1,5 @@
+name(['Test']);
+function name(params) {
+    let result = params[0].split('').reverse().join('');
+    console.log(result);
+    }
---
+++ JavScript/11. Strings/02.CorrectBrackets.js
@@ -0,0 +1,5 @@
+name(['((a+b)/5-d)']);
+function name(params) {
+    let result = params[0].split('').reverse().join('');
+    console.log(result);
+    }
